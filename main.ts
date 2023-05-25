5d88d52e7bbb990753a99f228523021f11d12a5d
BonnieH-SLSO <134567718+BonnieH-SLSO@users.noreply.github.com>
2023-05-25 12:21:06 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,65 @@
+let diceNumber = 0
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+})
 input.onGesture(Gesture.Shake, function () {
-    basic.showNumber(randint(1, 6))
+    basic.clearScreen()
+    diceNumber = randint(1, 7)
+    if (diceNumber == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (diceNumber == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . #
+            . . . . .
+            . . . . .
+            `)
+    } else if (diceNumber == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (diceNumber == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (diceNumber == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (diceNumber == 6) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
 })
